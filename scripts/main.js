fe3e68f18c3e29cc850220d0549e489a688ad1d1
Jon <jonols2009@gmail.com>
2023-07-28 15:46:03 -0600

    update-4

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,36 @@
 import { Tiles } from "/scripts/tileTemplate.js"
 
 let c = document.getElementById("main")
+let background = document.getElementById("map")
 c.width = window.innerWidth
 c.height = window.innerHeight
+background.width = window.innerWidth
+background.height = window.innerHeight
+let bgctx = background.getContext("2d")
 let ctx = c.getContext("2d")
 let tileSet = document.getElementById("tiles")
-let backdrop = new Tiles(ctx, tileSet, 10,20)
+
+let backdrop = new Tiles(bgctx, tileSet, 10,20)
+for(let i = 0; i<c.width; i++){
+        for(let b = 0; b<c.height; b++){
+            backdrop.draw(i,b)
+            
+        }
+    }
+        for(let i = -1; i<8; i++){
+            for(let b = 0; b<7; b++){
+            let layer = new Tiles(bgctx,tileSet, i,b)
+            layer.draw(i-2, b)
+        }
+        }
+        
+        for(let i = 0; i<5; i++){
+            for(let b = 7; b<12; b++){
+            let layer = new Tiles(bgctx,tileSet, i,b)
+            layer.draw(i+5, b-8)
+        }
+        }
+
 
 let controllerIndex= null;
 let leftPressed = false;
@@ -34,21 +59,20 @@ window.addEventListener("gamepaddisconnected", (event)=>{
 
 //     for(let i = 0; i<c.width; i++){
 //     for(let b = 0; b<c.height; b++){
-//     let backdrop = new Tiles(ctx, tileSet, 10,20)
 //         backdrop.draw(i,b)
         
 //     }
 // }
 //     for(let i = -1; i<8; i++){
 //         for(let b = 0; b<7; b++){
-//         let layer = new Tiles(ctx,tileSet, i,b)
+//         let layer = new Tiles(bgctx,tileSet, i,b)
 //         layer.draw(i-2, b)
 //     }
 //     }
     
 //     for(let i = 0; i<5; i++){
 //         for(let b = 7; b<12; b++){
-//         let layer = new Tiles(ctx,tileSet, i,b)
+//         let layer = new Tiles(bgctx,tileSet, i,b)
 //         layer.draw(i+5, b-8)
 //     }
 //     }
@@ -65,13 +89,15 @@ function controllerInput(){
     if(controllerIndex !== null){
         let gamepad = navigator.getGamepads()[controllerIndex]
         let buttons = gamepad.buttons
+        let leftrightvalue = gamepad.axes[0]
+
+        let stickDeadzone = 0.4
+        
         upPressed = buttons[12].pressed
         downPressed = buttons[13].pressed
         leftPressed = buttons[14].pressed
         rightPressed = buttons[15].pressed
 
-        let stickDeadzone = 0.4
-        let leftrightvalue = gamepad.axes[0]
 
         if(leftrightvalue >= stickDeadzone){
             rightPressed = true
@@ -104,16 +130,12 @@ playerUpdate()
 
     c.width = window.innerWidth
     c.height = window.innerHeight
-    for(let i = 0; i<c.width; i++){
-        for(let b = 0; b<c.height; b++){
-            backdrop.draw(i,b)
-            
-        }
-    }
+
 
 ctx.clearRect(player.x, player.y, player.width, player.height)
-//mapUpdate()
+ctx.drawImage(background, 0, 0, c.width, c.height, 0, 0, c.width, c.height)
 ctx.fillRect(player.x, player.y, player.width, player.height)
+
 requestAnimationFrame(update)
 }
 
